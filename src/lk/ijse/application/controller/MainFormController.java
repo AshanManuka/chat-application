38a05503e6db87532773789880ba43250aa4e34b
Manuka <manukajayarathne.coma@gmail.com>
2022-08-07 11:52:51 +0530

    Done login form

--- src/lk/ijse/application/controller/MainFormController.java
+++ src/lk/ijse/application/controller/MainFormController.java
@@ -1,4 +1,16 @@
 package lk.ijse.application.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public AnchorPane mainContext;
+    public JFXButton loginBtn;
+    public TextField userName;
+
+    public void loginAction(ActionEvent actionEvent) {
+
+    }
 }
