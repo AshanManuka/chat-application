b2a8e42645891baaae6b47851a9142816564195d
Manuka <manukajayarathne.coma@gmail.com>
2022-08-07 13:38:52 +0530

    make small functions

--- src/lk/ijse/application/controller/ChatFormController.java
+++ src/lk/ijse/application/controller/ChatFormController.java
@@ -1,5 +1,6 @@
 package lk.ijse.application.controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -7,14 +8,25 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 
-public class ChatFormController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ChatFormController implements Initializable {
     public AnchorPane chatContext;
     public TextArea chatBox;
     public Label boxNameLbl;
     public TextField typeField;
     public ImageView galleryId;
+    public static String chatName;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        boxNameLbl.setText(chatName);
+    }
 
     public void galleryAction(MouseEvent mouseEvent) {
     System.out.println("clicked camera");
     }
+
 }
--- src/lk/ijse/application/controller/MainFormController.java
+++ src/lk/ijse/application/controller/MainFormController.java
@@ -3,25 +3,46 @@ package lk.ijse.application.controller;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainFormController {
+public class MainFormController implements Initializable {
     public AnchorPane mainContext;
     public JFXButton loginBtn;
     public TextField userName;
 
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        userName.requestFocus();
+    }
+
     public void loginAction(ActionEvent actionEvent) throws IOException {
+        goChatPage();
+    }
+
+    public void goChatPage() throws IOException {
+        ChatFormController.chatName = userName.getText();
+        userName.clear();
         Parent parent = FXMLLoader.load(getClass().getResource("../view/chatForm.fxml"));
         Stage stage1 = new Stage();
         stage1.setScene(new Scene(parent));
         stage1.show();
+    }
 
-
+    public void enterEvent(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER){
+            goChatPage();
+        }
     }
 }
